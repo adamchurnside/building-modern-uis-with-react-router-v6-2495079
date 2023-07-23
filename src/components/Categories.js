433ec9cdebabfdb2b1fb003f3e4f314ed73d0911
Adam Churnside <adam.churnside@gmail.com>
2023-07-22 18:53:18 -0700

    initial commit

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,17 @@
+import { getCategories } from "../api";
+
 export default function Categories() {
+
+  const categories = getCategories();
+
   return (
     <div className="container">
       <h1>Session Categories</h1>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
